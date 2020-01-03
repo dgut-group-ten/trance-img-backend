63c036de5298261b97f7af568c6bf24e2ee40351
Niracler <1026037967@qq.com>
2020-01-03 16:29:39 +0800

    demo

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,23 @@
 import axios from 'axios';
 
-export const flaskHost = process.env.VUE_APP_FLASK_HOST;
-export const proHost = process.env.VUE_APP_PRO_HOST;
-export const host = proHost;
+export const host = 'http://192.168.1.109:8080';
 
-//获取游戏实体信息
-export const entityInfo = params => {
-    return axios.get(`${flaskHost}/api/entity/`, params) //原来这里就用到了自定义插值，模板字符串以及箭头定义函数
+export const allImages = params => {
+    return axios.get(`${host}/getFiles`, params) //原来这里就用到了自定义插值，模板字符串以及箭头定义函数
+};
+
+export const idCardRecognize = params => {
+    return axios.get(`${host}/IDCardRecognize`, params)
+};
+
+export const plateLicense = params => {
+    return axios.get(`${host}/plateLicense`, params)
+};
+
+export const imgResult = params => {
+    return axios.get(`${host}/result`, params)
+};
+
+export const receipt = params => {
+    return axios.get(`${host}/receipt`, params)
 };
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -3,7 +3,7 @@
         <!-- End Row -->
         <div class="row">
             <div class="col-12 m-b-30">
-                <h2 class="d-inline">文字功能体验</h2>
+                <h2 class="d-inline">文字识别功能体验</h2>
                 <p class="text-muted">This is 3 column contents</p>
 
                 <div class="card">
@@ -23,21 +23,39 @@
                                      :key="op._id">
                                     <div class="row">
                                         <div class="col-lg-6">
-                                            <img :src="cur_img.img_path" class="img-fluid" alt="Responsive image"
+                                            <img :src="cur_img.url" class="img-fluid" alt="Responsive image"
                                                  data-toggle="modal" data-target="#exampleModalScrollable">
-                                            <a :href="cur_img.img_path">{{ cur_img.img_path.split('/')[5] }}</a>
+                                            <div class="my-4">
+                                                <a :href="cur_img.url">{{ cur_img.url.split('/')[4] }}</a>
+                                            </div>
                                         </div>
                                         <div class="col-lg-6">
-                                            <div class="p-t-15">
-                                                <h4>{{ op.name }}结果</h4>
-                                                <p>Far far away, behind the word mountains, far from the countries
-                                                    Vokalia and
-                                                    Consonantia, there live the blind texts. Separated they live in
-                                                    Bookmarksgrove.</p>
-                                                <p>Far far away, behind the word mountains, far from the countries
-                                                    Vokalia and
-                                                    Consonantia, there live the blind texts. Separated they live in
-                                                    Bookmarksgrove.</p>
+                                            <div class="p-t-15 container">
+                                                <h2>{{ op.name }}结果</h2>
+                                                <div class="p-3 text-left">
+                                                    <div v-if="op.name === '车牌识别' && res">
+                                                        <h4>车牌号: {{res.number}}</h4>
+                                                        <h4>颜色: {{res.color}}</h4>
+                                                        <h4>四个角的位置</h4>
+                                                        <h5 v-for="point in res.vertexes_location" :key="point._id">
+                                                            {{point}}
+                                                        </h5>
+                                                    </div>
+                                                    <div v-if="op.name === '发票识别' && res">
+                                                        <h5 v-for="i in res" :key="i._id">
+                                                            {{i.words}}
+                                                        </h5>
+                                                    </div>
+                                                    <div v-if="op.name === '身份证识别' && res">
+                                                        <h4 v-for="(v, k) in res" :key="k._id">
+                                                            {{k}}: {{v.words}}
+                                                        </h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="my-4">
+                                                <input class="btn login-form__btn submit w-75"
+                                                       @click="getResult(cur_img, op)" value="开始识别">
                                             </div>
                                         </div>
                                     </div>
@@ -62,14 +80,15 @@
                                 <div class="row">
                                     <div class="col-md-6 col-lg-6" v-for="image in images" :key="image._id">
                                         <div class="card">
-                                            <img class="img-fluid" :src="image.img_path" alt="" @click="select_img(image)">
+                                            <img class="img-fluid" :src="image.url" alt="" @click="select_img(image)">
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
-                                <button type="button" class="btn btn-primary" data-dismiss="modal">确定</button>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal">确定
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -82,91 +101,60 @@
 </template>
 
 <script>
+    import {allImages, plateLicense, idCardRecognize, receipt} from "../api";
+
     export default {
         name: "Gallery",
         data() {
             return {
                 cur_img: {
-                    name: "1",
-                    img_path: "https://music-01.niracler.com:8002/media/cimg/20200103_093212.png",
+                    id: 0,
+                    url: "https://music-01.niracler.com:8002/media/cimg/20200103_093212.png"
                 },
                 ops: [
-                    {name: "文字卡片内容识别", val: "op1", active: "active"},
-                    {name: "车牌识别", val: "op2", active: ""},
-                    {name: "发票识别", val: "op3", active: ""},
-                    {name: "身份证识别", val: "op4", active: ""},
+                    {name: "车牌识别", val: "op2", active: "active", fun:plateLicense},
+                    {name: "发票识别", val: "op3", active: "", fun:receipt},
+                    {name: "身份证识别", val: "op4", active: "", fun:idCardRecognize},
                 ],
-                images: [
-                    {
-                        name: "1",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "2",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "3",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "4",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "5",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "6",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "7",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "8",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "9",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "10",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "11",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "12",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "13",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "14",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                    {
-                        name: "15",
-                        img_path: "https://music-01.niracler.com:8002/media/cimg/7b86d2ffd3fd4cc5a7129b5ae909fa84.jpg"
-                    },
-                ],
-                totalPage: 1,
-                page: 1,
+                images: null,
+                res: null,
             }
         },
         methods: {
-            select_img(path){
+            select_img(path) {
                 this.cur_img = path
+            },
+            getImages() {
+                allImages({}).then(response => {
+                    if (response.status === 200) {
+                        this.images = response.data.data;
+                    } else {
+                        alert('获取数据失败！！！')
+                    }
+                }).catch(function (error) { // 请求失败处理
+                    self.console.log(error);
+                });
+            },
+            getResult(img, op) {
+                op.fun({
+                    params: {
+                        id: img.id
+                    }
+                }).then(response => {
+                    if (response.status === 200) {
+                        self.console.log(response.data)
+                        this.res = response.data.words_result;
+                    } else {
+                        alert('获取数据失败！！！')
+                    }
+                }).catch(function (error) { // 请求失败处理
+                    self.console.log(error.response);
+                });
             }
-        }
+        },
+        created() {
+            this.getImages();
+        },
     }
 </script>
 
